dff9d6463a272caff5f9fee934a3c3ec94779a97
hangyudu <hangyudu@tencent.com>
2021-11-08 11:57:41 +0800

    add test code

---
+++ 036/kubernetes/kubia/app.js
@@ -0,0 +1,13 @@
+const http = require('http');
+const os = require('os');
+
+console.log("Kubia server starting...");
+
+var handler = function(request, response) {
+  console.log("Received request from " + request.connection.remoteAddress);
+  response.writeHead(200);
+  response.end("You've hit v2 " + os.hostname() + "\n");
+};
+
+var www = http.createServer(handler);
+www.listen(8080);
